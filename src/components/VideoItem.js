c3b1ef5b9d2a6a1a8117249cc5f52df639df8fb3
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2022-09-05 02:58:07 -0400

    completed

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,19 +1,19 @@
-import "./VideoItem.css"
+import "./VideoItem.css";
 import React from "react";
 
-const VideoItem = ({ video }) => {
-  console.log(video.id);
-
+const VideoItem = ({ video, onVideoSelect }) => {
   return (
-    <div className="video-item item">
-      <strong>
-        <a className="header">{video.snippet.title}</a>
-      </strong>
+    <div onClick={() => onVideoSelect(video)} className="video-item item">
       <img
         className="ui image"
         src={video.snippet.thumbnails.medium.url}
-        alt="video item"
+        alt={video.snippet.title}
       />
+      <div className="content">
+        <strong>
+          <div className="header">{video.snippet.title}</div>
+        </strong>
+      </div>
     </div>
   );
 };
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,10 +1,16 @@
 import React from "react";
 import VideoItem from "./VideoItem";
 
-const VideoList = ({ videos }) => {
-  console.log(videos)
+const VideoList = ({ videos, onVideoSelect }) => {
+  console.log(videos);
   const renderedList = videos.map((video) => {
-    return <VideoItem video={video} />;
+    return (
+      <VideoItem
+        key={video.id.videoId}
+        onVideoSelect={onVideoSelect}
+        video={video}
+      />
+    );
   });
   return <div className="ui relaxed divided list">{renderedList}</div>;
 };
